57f897a7f7b681cc7cb4803632082348f5c8d128
Yakov Gellert <68142933+novusnota@users.noreply.github.com>
2021-04-03 00:59:37 +0200

    Temporal changes

--- conti.sh
+++ conti.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+# reset getopts
+OPTIND=1
 
+# handle options
+TEMP=$(\
+    getopt -o hbrkwels:c:v:f:p:\
+    --long help,write,keep,reset,bold,format:,script:,color:,print:\
+    -n "$FUNCNAME" -- "$@"
+)
+
+while getopts "" opt; do
+
+    case "$opt" in
+
+        h)
+        ;;
+    isac
+done
 
 exit 0
+
